4f2cd2c6da0c2957eef522241004e790b782ed0e
Celeste <daikimeitareas@gmail.com>
2025-08-22 12:58:37 -0600

    Home and section about me added

--- app/(home)/components/home.jsx
+++ app/(home)/components/home.jsx
@@ -1,10 +1,12 @@
 export default function Presentation() {
   return (
     <div class="relative w-2xl h-2xl mx-auto">
-      <div class="flex flex-row dark:border-gray-600 outline-1 outline-gray-600 rounded-lg">
-        <div class="basis-1/2 flex p-10 ">
-          <p>
-            I am a<span class="font-bold"> Computer systems engineer</span>
+      <h2 className="text-[40px] mb-2 ">About Me</h2>
+
+      <div className="flex flex-row dark:border-gray-600 outline-1 outline-gray-600 rounded-lg">
+        <div className="basis-1/2 flex p-10 text-center text-lg">
+          <p className="font-extralight">
+            I am a<span className="font-bold"> Computer systems engineer </span>
             developer with experience building web applications. My work is
             based on technologies such as React, Next.js, JavaScript, Laravel,
             PHP, Tailwind CSS and Bootstrap. I have managed databases with MySQL
@@ -12,10 +14,10 @@ export default function Presentation() {
           </p>
         </div>
       </div>
-      <div class="absolute right-[-8.5rem] top-1/2 -translate-y-1/2">
+      <div class="absolute right-[-10rem] top-50 -translate-y-1/3">
         <img
           src="../celes.png"
-          class="rounded-full shadow-[0_20px_80px_rgba(5,_112,_184,_0.7)] shadow-blue-800/20 w-72"
+          class="rounded-full shadow-[0_20px_80px_rgba(5,_112,_184,_0.7)] shadow-blue-800/20 w-82"
         />
       </div>
     </div>
--- app/(home)/page.js
+++ app/(home)/page.js
@@ -4,7 +4,10 @@ import Presentation from "./components/home";
 export default function Home() {
   return (
     <div>
-      <div className="m-10">
+      <div className="flex justify-center my-20">
+        <p className="text-[50px] font-bold">Hi, I'm Celeste</p>
+      </div>
+      <div className="m-10 my-20">
         <Presentation />
       </div>
     </div>
