c0138f85dd4e8037bc9c8c2ce51ec3e06272c862
Celeste <daikimeitareas@gmail.com>
2025-09-04 11:55:17 -0600

    Technologies with icons  added

--- app/(home)/components/experiencie.jsx
+++ app/(home)/components/experiencie.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { GoArrowUpRight } from "react-icons/go";
+import Technologies from "./technologies";
 
 export default function Experiencie() {
   const experiences = [
@@ -10,7 +11,21 @@ export default function Experiencie() {
       description:
         "Designed and developed a web application for internal process management. Implemented secure authentication and role management using NextAuth. Worked with PostgreSQL and Prisma ORM; deployed the app using Vercel. Implemented technologies: Next.js, Shadcn UI, TailwindCSS, Zod. Applied version control and project management using Git, GitHub, and Jira. Followed Agile methodologies such as Scrum for team collaboration. Integrated third-party services like UploadThing for image handling ",
       date: "Jalisco, México  January – June 2025  ",
+      technologies: [
+        "JavaScript",
+        "PostgreSQL",
+        "Prisma",
+        "ShaddcnUI",
+        "TailwindCss",
+        "Zod",
+        "Git",
+        "GitHub",
+        "Jira",
+        "ReactHookForm",
+        "Next",
+      ],
     },
+
     {
       title: "GYM",
       position: "Full Stack Developer – Academic Project",
@@ -18,6 +33,14 @@ export default function Experiencie() {
         "Developed a subscription payment platform for a gym. Implemented JWT-based authentication. Integrated payment gateway using Mercado Pago. Used React.js for the frontend and Node.js with Express for the backend. Database: MySQL.",
 
       date: "Jalisco, México  January – June 2025  ",
+      technologies: [
+        "JavaScript",
+        "MySQL",
+        "React",
+        "Node",
+        "Express",
+        "Bootstrap",
+      ],
     },
     {
       title: "Web Application Project",
@@ -25,6 +48,7 @@ export default function Experiencie() {
       description:
         "Built a web application using React and Bootstrap for the frontend. Backend developed with Laravel and PHP, consuming external APIs. Implemented pagination to enhance navigation and user experience. Used Bootstrap to create a responsive web interface.",
       date: "Jalisco, México  August – December 2024   ",
+      technologies: ["Laravel", "PHP", "Bootstrap", "JavaScript", "React"],
     },
   ];
 
@@ -42,20 +66,20 @@ export default function Experiencie() {
 
       <div className="container w-auto mx-auto sm:max-w-7xl p-4 ">
         <div className="grid grid-cols-1 grid-rows-auto gap-6">
-          {experiences.map((experiencie, index) => (
+          {experiences.map((experience, index) => (
             <div
               className="bg-slate-800/50 border-slate-700 hover:border-blue-500/50 transition-all duration-300 hover:shadow-2xl hover:shadow-blue-500/10 backdrop-blur-sm relative border rounded-md p-4 flex flex-col w-full lg:w-9/12 mx-auto"
               key={index}
             >
               <div className="flex flex-col sm:flex-row justify-between ">
                 <div className="text-xl font-bold text-white ">
-                  {experiencie.title}
+                  {experience.title}
                   <div className="text-gray-200 font-extralight text-sm ">
-                    {experiencie.position}
+                    {experience.position}
                   </div>
                 </div>
                 <div className="text-sm text-white justify-end mb-5">
-                  {experiencie.date}
+                  {experience.date}
                 </div>
               </div>
 
@@ -63,9 +87,9 @@ export default function Experiencie() {
 
               <div className="flex-grow overflow-hidden ">
                 <div className="rounded-md p-1">
-                  {experiencie.description && (
+                  {experience.description && (
                     <ul className="list-blue-marker p-2 list-disc list-inside text-white space-y-1">
-                      {experiencie.description
+                      {experience.description
                         .split(". ")
                         .map(
                           (valor, i) =>
@@ -75,11 +99,17 @@ export default function Experiencie() {
                   )}
                 </div>
               </div>
-              <div className="mt-4 flex justify-end ">
-                <Button className="group bg-transparent border border-blue-500/50 text-blue-300 hover:bg-blue-500/10 hover:border-blue-400 hover:text-blue-200 transition-all duration-200 ">
-                  <span>View Gallery</span>
-                  <GoArrowUpRight className="transition-transform duration-300 group-hover:translate-x-0.5" />
-                </Button>
+              <div className="mt-4 flex justify-between items-end sm:flex-col sm:items-center sm:gap-4">
+                <div className="mb-4 sm:mb-0">
+                  <Technologies technologies={experience.technologies} />
+                </div>
+
+                <div>
+                  <Button className="group bg-transparent border border-blue-500/50 text-blue-300 hover:bg-blue-500/10 hover:border-blue-400 hover:text-blue-200 transition-all duration-200 ">
+                    <span>View Gallery</span>
+                    <GoArrowUpRight className="transition-transform duration-300 group-hover:translate-x-0.5" />
+                  </Button>
+                </div>
               </div>
             </div>
           ))}
---
+++ app/(home)/technologies.js
@@ -0,0 +1,39 @@
+import { FaBootstrap } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { BiLogoPostgresql } from "react-icons/bi";
+import { SiPostgresql, SiPrisma } from "react-icons/si";
+import { SiShadcnui } from "react-icons/si";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { SiZod } from "react-icons/si";
+import { FaGitAlt } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { SiJira } from "react-icons/si";
+import { SiMysql } from "react-icons/si";
+import { FaReact } from "react-icons/fa6";
+import { FaNodeJs } from "react-icons/fa";
+import { SiExpress } from "react-icons/si";
+import { SiReacthookform } from "react-icons/si";
+import { FaLaravel } from "react-icons/fa";
+import { FaPhp } from "react-icons/fa";
+import { RiNextjsFill } from "react-icons/ri";
+
+export const icons = {
+  JavaScript: <IoLogoJavascript />,
+  PostgreSQL: <BiLogoPostgresql />,
+  Prisma: <SiPrisma />,
+  ShaddcnUI: <SiShadcnui />,
+  TailwindCss: <RiTailwindCssFill />,
+  Zod: <SiZod />,
+  Git: <FaGitAlt />,
+  GitHub: <FaGithub />,
+  Jira: <SiJira />,
+  MySQL: <SiMysql />,
+  React: <FaReact />,
+  Node: <FaNodeJs />,
+  Express: <SiExpress />,
+  ReactHookForm: <SiReacthookform />,
+  Bootstrap: <FaBootstrap />,
+  Laravel: <FaLaravel />,
+  PHP: <FaPhp />,
+  Next: <RiNextjsFill />,
+};
